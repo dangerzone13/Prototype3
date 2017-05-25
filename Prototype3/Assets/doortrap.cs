4ca7cadd4e48231d9f37e7358c200bc9da501c02
AIENWARE-762123\User <dannisquiggles>
2017-05-25 22:58:09 +1000

    trap door begun

---
+++ Prototype3/Assets/doortrap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doortrap : MonoBehaviour {
+    public float maxpos;
+    public float minpos;
+    public float speed;
+
+    private bool stop;
+    private bool open;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open = true && transform.position.x < 0)
+        {
+            transform.position.x -= speed;
+        }
+	}
+}

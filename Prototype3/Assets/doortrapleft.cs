824a79e6bf94a0c88cc424d2a898ac89fd31fdbb
AIENWARE-762123\User <dannisquiggles>
2017-05-26 03:03:06 +1000

    trap door finished.
    trap door opens and closes on code. it stops when it collides with
    player shield. colliders and damage are set, but the player has to have
    damage read on it.

---
+++ Prototype3/Assets/doortrapleft.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doortrapleft : MonoBehaviour {
+    public float maxpos;
+    private float minpos;
+    private float newmax;
+    public float speed;
+
+    public bool open;
+    public bool stopped;
+    // Use this for initialization
+    void Start () {
+        open = false;
+        minpos = transform.position.x;
+        newmax = minpos + maxpos;
+        stopped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open == true && transform.position.x <= minpos && stopped == false)
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+            if (transform.position.x >= minpos)
+            {
+                open = false;
+            }
+        }
+
+        if (open == false && transform.position.x >= newmax && stopped == false)
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+            if (transform.position.x <= newmax)
+            {
+                open = true;
+            }
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Shield")
+        {
+            stopped = true;
+        }
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Shield")
+        {
+            stopped = true;
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Shield")
+        {
+            stopped = false;
+        }
+    }
+
+}

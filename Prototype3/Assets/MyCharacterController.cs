2465202fc13c214c3e98ff2d47d947e7f2412c3f
Adrian Randy Satrianugraha <adrianrandy77013@gmail.com>
2017-05-30 22:32:28 +1000

    More Changes to Custom Controller + More Animations Implemented

--- Prototype3/Assets/MyCharacterController.cs
+++ Prototype3/Assets/MyCharacterController.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class MyCharacterController : MonoBehaviour 
 {
-	private GameObject player;
-	public float speed;
+	public float speed = 2f;
 	public float rotationSpeed = 2.0f;
-	public Transform head;
 	public bool moving;
+
+	public float jumpSpeed = 10.5f;
+	public float gravity = 30.0f;
 
 	public float maxHeadRotation = 80.0f;
 	public float minHeadRotation = -80.0f;
-	private float currentHeadRotation = 0;
 
+	public Transform head;
+
+	private float currentHeadRotation = 0;
 	private float yVelocity = 0;
-	public float jumpSpeed = 5.0f;
-	public float gravity = 30.0f;
+
+	private CharacterController controller;
+	private Vector3 moveVelocity = Vector3.zero;
 
 	public bool aim; 
-	public GameObject shield;
+	public bool shield;
 
 	private Animator anim;
 	// Use this for initialization
@@ -30,6 +34,7 @@ public class MyCharacterController : MonoBehaviour
 		//Set Grounded Anim Bool to True
 		anim.SetBool("Grounded", true);
 
+		controller = GetComponent<CharacterController>();
 
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
@@ -44,60 +49,100 @@ public class MyCharacterController : MonoBehaviour
 			Cursor.visible = true;
 		}
 
-		//Jump Input
-		if (Input.GetButtonDown("Jump"))
+		Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+		if(!controller.isGrounded)
 		{
-			yVelocity = jumpSpeed;
+			Debug.Log ("Grounded");
+			//yVelocity = 0;
+
+			//Jump Input
+			if (Input.GetButtonDown("Jump"))
+			{
+				Debug.Log ("Jump");
+				yVelocity = jumpSpeed;
+			}
+				/*anim.SetBool ("Grounded", false);
+				anim.SetBool ("Jump", true);
+			} 
+			else 
+			{ 
+				anim.SetBool ("Grounded", true);
+				anim.SetBool ("Jump", false);
+			}*/
+
+			moveVelocity = transform.TransformDirection(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"))) * speed;
 		}
 
 		yVelocity -= gravity * Time.deltaTime;
 
 		//Mouse Movement & Head Rotation
-		Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 		transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
 		currentHeadRotation = Mathf.Clamp(currentHeadRotation + mouseInput.y * rotationSpeed, minHeadRotation, maxHeadRotation);
 		head.localRotation = Quaternion.identity;
 		head.Rotate(Vector3.left, currentHeadRotation);
 
 		//Character Movement
-		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-		gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
-
+		Vector3 velocity = moveVelocity + yVelocity * Vector3.zero;
+		controller.Move(velocity * Time.deltaTime);
+		//Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		//gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+	
 
 		//Activating Walk Cycle Animation
 		if (Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0) 
 		{
 			anim.SetBool ("Walking", true);
-			Debug.Log ("Vertical");
+			moving = true;
 		} 
 		else 
 		{
 			anim.SetBool ("Walking", false);
+			moving = false;
+		}
+
+		//Animator Bool for Block Movement
+		if (moving) 
+		{
+			anim.SetBool ("Move Blocking", true);
+		}
+		else 
+		{
+			anim.SetBool ("Move Blocking", false);
 		}
 
 		//Shield Block
-		// Activate aim by input.
 		aim = Input.GetButton("Aim");
 
 		// Player is aiming.
 		if (Input.GetButton ("Aim")) 
 		{
-			//Shield Activates
-			shield.SetActive (true);
-
 			//Block Animator Bool set to True
 			anim.SetBool ("Blocking", true);
-
-			if (Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0) 
-			{
-				//Blocking Animator Bool set to True
-				anim.SetBool ("Move Blocking", true);
-			}
 		} 
 		else 
 		{
 			anim.SetBool ("Blocking", false);
 			anim.SetBool ("Move Blocking", false);
+		}
+
+		//If Shield is Active
+		if (AimBehaviour.shield) 
+		{
+			shield = true;
+		} 
+		else 
+		{
+			shield = false;
+		}
+
+		if (aim) 
+		{
+			speed = 0.5f;
+		} 
+		else 
+		{
+			speed = 2.0f;
 		}
 	}
 }
